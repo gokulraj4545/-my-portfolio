c9501ab43e85a1e3e1db782233ea92c48eda4d3e
gokulraj4545 <gokulraj922005@gmail.com>
2025-07-05 12:14:39 +0530

    file update

--- script.js
+++ script.js
@@ -11,3 +11,24 @@ document.getElementById("contactForm")?.addEventListener("submit", function(e) {
     document.getElementById("formResponse").innerText = "Please fill in all fields.";
   }
 });
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzESJsi3hO51zCnl7OvcxA7HBku2Lm8uwU7K2gGSKHR6xueGSngRE530NyUrx6OJEDo/exec';
+
+document.getElementById('contactForm').addEventListener('submit', e => {
+  e.preventDefault();
+  
+  const form = e.target;
+
+  fetch(scriptURL, {
+    method: 'POST',
+    body: JSON.stringify({
+      name: form.name.value,
+      email: form.email.value,
+      message: form.message.value
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(response => alert("✅ Message sent successfully!"))
+  .catch(error => alert("❌ Something went wrong."));
+});
