f5e2c21ba7f7668c56ae71787c6081665164882a
gokulraj4545 <gokulraj922005@gmail.com>
2025-07-05 12:18:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-document.getElementById("contactForm")?.addEventListener("submit", function(e) {
-  e.preventDefault();
-  const name = document.getElementById("name").value.trim();
-  const email = document.getElementById("email").value.trim();
-  const message = document.getElementById("message").value.trim();
 
-  if (name && email && message) {
-    document.getElementById("formResponse").innerText = `Thank you, ${name}! Your message has been received.`;
-    this.reset();
-  } else {
-    document.getElementById("formResponse").innerText = "Please fill in all fields.";
-  }
-});
 const scriptURL = 'https://script.google.com/macros/s/AKfycbzESJsi3hO51zCnl7OvcxA7HBku2Lm8uwU7K2gGSKHR6xueGSngRE530NyUrx6OJEDo/exec';
 
 document.getElementById('contactForm').addEventListener('submit', e => {
